1e66df2bdcfcfa2d149a2e9ab7f3cdf331152720
Justin Moran <kyaneos@mac.lan>
2025-10-25 20:37:53 -0700

    Add prerender entries for all dynamic routes

--- svelte.config.js
+++ svelte.config.js
@@ -19,6 +19,29 @@ const config = {
 		paths: {
 			// Base path for GitHub Pages (username.github.io/repo-name)
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || ''
+		},
+		prerender: {
+			handleMissingId: 'warn',
+			entries: [
+				'*',
+				'/course/1',
+				'/course/2',
+				'/course/3',
+				'/lesson/1',
+				'/lesson/2',
+				'/lesson/3',
+				'/lesson/4',
+				'/lesson/5',
+				'/lesson/6',
+				'/lesson/7',
+				'/quiz/1',
+				'/quiz/2',
+				'/quiz/3',
+				'/quiz/4',
+				'/quiz/5',
+				'/quiz/6',
+				'/quiz/7'
+			]
 		}
 	}
 };
